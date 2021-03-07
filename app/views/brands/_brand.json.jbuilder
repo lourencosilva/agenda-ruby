630645a3ee3a8458f0a08ea61153c28052e850bf
Lourenço Silva Júnior <lourenco.silva72@gmail.com>
2021-03-07 07:43:18 -0300

    Create tables Brand and Category

---
+++ app/views/brands/_brand.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! brand, :id, :description, :product_id, :created_at, :updated_at
+json.url brand_url(brand, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_103554) do
+ActiveRecord::Schema.define(version: 2021_03_07_104229) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string "description"
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_brands_on_product_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "description"
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_categories_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "description"
@@ -21,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_03_07_103554) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "brands", "products"
+  add_foreign_key "categories", "products"
 end
